d975a4a564ece57fe4a2062034ad87b541db2b93
Matteo <skewre>
2020-03-06 17:26:09 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const bcrypt = require("bcryptjs")
+const app = express()
+const port = 3000
+
+// link live:
+// https://www.twitch.tv/videos/562180505
+
+// Minuto: 2:19:41
+
+// middleware setup
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("dev"))
+
+const low = require("lowdb")
+const FileSync = require("lowdb/adapters/FileSync")
+
+const adapter = new FileSync("db.json")
+const db = low(adapter)
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] }).write()
+
+app.get("/", (req, res) => res.send("Hello World!"))
+app.post("/users/:username", async (req, res) => {
+  const password = req.body.password
+  const username = req.params.username
+  const user = db
+    .get("users")
+    .find({ username })
+    .value()
+
+  const authenticated = await bcrypt.compare(password, user.hashedPassword)
+
+  res.send({ authenticated })
+})
+
+app.post("/users", async (req, res) => {
+  const { username, password } = req.body
+
+  if (
+    db
+      .get("users")
+      .find({ username: username })
+      .size()
+      .value() > 0
+  ) {
+    res.status(400).send({
+      error: "user already exists"
+    })
+    return
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 8)
+
+  const newUser = {
+    username,
+    hashedPassword
+  }
+
+  db.get("users")
+    .push(newUser)
+    .write()
+
+  res.send(newUser)
+})
+
+app.listen(port, () => console.log(`The app is listening on port ${port}!`))
---
+++ tpi.md
@@ -0,0 +1,12 @@
+# Lavoro Coronese TPI
+
+## Server REST
+
+Realizzare un mockup di un sistema client-server per l'autenticazione.
+Il client manderà le credenziali al server ed otterrà da esso un TOKEN ad avvenuta autenticazione. Il TOKEN sarà necessario per tutte le altre chiamate da parte del client verso il server.
+
+### Necessario
+
+- Un DB di utenti (andrà usato lowdb)
+- Un sistema di autenticazione
+- Un sistema di gestione del webtoken (andrà usato Express)
