eea117b4d3f49c8a884f63a0ddf90de3b7fba2bc
Matteo-x220 <matteosasoris@gmail.com>
2020-03-13 20:35:08 +0100

    First deployment attempt

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const helmet = require("helmet")
-const morgan = require("morgan")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const app = express()
-const port = 3000
+// const port = 3000
+const port = process.env.PORT || 3000
 
 // middleware setup
 app.use(helmet())
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
-const low = require("lowdb")
-const FileSync = require("lowdb/adapters/FileSync")
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync("db.json")
+const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 // Set some defaults (required if your JSON file is empty)
 db.defaults({ users: [] }).write()
 
-app.get("/", (req, res) => res.send("Hello World!"))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 const validateToken = async (req, res) => {
-  const token = req.headers["x-access-token"]
+  const token = req.headers['x-access-token']
   if (!token) {
-    console.log("Token is undefined")
+    console.log('Token is undefined')
     res.status(403).json({
-      error: "unauthorized"
+      error: 'unauthorized'
     })
     return
   }
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
-      console.log("Error with the token", err)
+      console.log('Error with the token', err)
       res.status(403).json({
-        error: "unauthorized"
+        error: 'unauthorized'
       })
       return
     }
   })
 }
 
-app.get("/login", async (req, res) => {
+app.get('/login', async (req, res) => {
   validateToken(req, res)
-  res.send({ status: "ok" })
+  res.send({ status: 'ok' })
 })
 
-app.post("/users/:username", async (req, res) => {
+app.post('/users/:username', async (req, res) => {
   const password = req.body.password
   const username = req.params.username
 
   const user = db
-    .get("users")
+    .get('users')
     .find({ username })
     .value()
 
@@ -67,21 +68,21 @@ app.post("/users/:username", async (req, res) => {
     authenticated,
     token
   })
-  console.log("Token sent")
+  console.log('Token sent')
 })
 
-app.post("/users", async (req, res) => {
+app.post('/users', async (req, res) => {
   const { username, password } = req.body
 
   if (
     db
-      .get("users")
+      .get('users')
       .find({ username: username })
       .size()
       .value() > 0
   ) {
     res.status(400).send({
-      error: "user alredy exists"
+      error: 'user alredy exists'
     })
     return
   }
@@ -93,11 +94,11 @@ app.post("/users", async (req, res) => {
     hashedPassword
   }
 
-  db.get("users")
+  db.get('users')
     .push(newUser)
     .write()
 
-  console.log("New user added:", newUser.username)
+  console.log('New user added:', newUser.username)
 
   res.send(newUser)
 })
